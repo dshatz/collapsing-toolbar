8e1465817ca00cb0e241dcab67fe5ca21b732c21
Daniels Šatcs <dev@dshatz.com>
2025-08-07 05:19:36 +0300

    Add desktop app

--- app/src/commonMain/kotlin/com/dshatz/collapsingtoolbar/App.kt
+++
@@ -1,6 +0,0 @@
-package com.dshatz.collapsingtoolbar
-
-
-fun main() {
-
-}
---
+++ app/src/jvmMain/kotlin/com/dshatz/collapsingtoolbar/App.kt
@@ -0,0 +1,14 @@
+package com.dshatz.collapsingtoolbar
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() {
+
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            Root()
+        }
+    }
+}
